4cc86fd4eaf7a017502fb0001869b8bff65a840a
Lu Zhao <lzhao819@usc.edu>
2020-09-02 21:46:01 -0700

    add files

--- LongestPalindromicSubstring.java
+++ LongestPalindromicSubstring.java
@@ -3,22 +3,29 @@
 //Time O(N^2) 
 class Solution {
     public String longestPalindrome(String s) {
-        
-        int max = 0, idx = 0;
-        for (int i = 0; i < s.length(); i++) {
-            int len1 = extend(s, i, i), len2 = extend(s, i, i + 1);
-            if (max < Math.max(len1, len2)) {
-                idx = (len1 > len2) ? (i - len1 / 2) : (i - len2 / 2 + 1);
-                max = Math.max(len1, len2);
-            }
-        }
-        return s.substring(idx, idx + max);
-    }
-    
-    private int extend(String s, int i, int j) {
-        for (; i >= 0 && j < s.length(); i--, j++)
-            if (s.charAt(i) != s.charAt(j))
-                break;
-        return j - i - 2 + 1; // 2 means current two unmatched char
+        String max = "";
+        if(s==null ||s.length()<1) return max;
+		for(int i = 0; i < s.length(); i++){
+			String s1 = extend(s, i, i);
+			String s2 = extend(s, i, i + 1);
+			if(s1.length() > max.length()){
+				max = s1;
+			}
+			if(s2.length() > max.length()){
+				max = s2;
+			}
+		}
+		return max;
+	}
+	public static String extend(String s, int i, int j){
+		while(i >= 0 && j < s.length()){
+			if(s.charAt(i) != s.charAt(j)){
+				break;
+			}
+			i--;
+			j++;
+		}
+
+		return s.substring(i + 1, j);
     }
 }
