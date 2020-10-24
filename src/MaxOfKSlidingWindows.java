e7517345c4fcdf4fbdcca4960b598da6c10e00d1
Lu Zhao <lzhao819@usc.edu>
2020-10-24 01:17:51 -0700

    jianzhi

--- src/MaxOfKSlidingWindows.java
+++ src/MaxOfKSlidingWindows.java
@@ -14,26 +14,27 @@ import java.util.Deque;
 //time O(n) - amortized for each step O(1), worse case O(k)
 //Space O(n)
 public class MaxOfKSlidingWindows {
-    public static int[] maxWindows(int[] array, int k) {
+    public static int[] maxWindows(int[] nums, int k) {
       //using a deque to keep track of max value
       //the deque will be in descending order and the first element is the result
       //save int index rather than the real value in the deque
       //make sure deque only contains the index in current slidng window
       //for any index, the prev index with smaller value will be discard from deque
-      int[] result = new int[array.length-k+1];
-      Deque<Integer> deque = new ArrayDeque<>();
-      //discard smaller value
-      for(int i=0;i<array.length;i++){
-        while(!deque.isEmpty() && array[deque.peekLast()]<=array[i]){
+      //递减序列
+      Deque <Integer> deque = new ArrayDeque<>();
+      int[] result = new int[nums.length-k+1];
+      for(int i=0;i<nums.length;i++){
+        //去掉小的
+        while(!deque.isEmpty()&& nums[deque.peekLast()]<=nums[i]){
           deque.pollLast();
         }
-        //discard the head value wich are out of the sliding window
-        if(!deque.isEmpty() && deque.peekFirst()<=i-k){
+        //去掉出界的
+        if(!deque.isEmpty() && deque.peekFirst()<i-k+1){
           deque.pollFirst();
         }
         deque.offerLast(i);
-        if(i>=k-1){
-          result[i-k+1] = array[deque.peekFirst()];
+        if(i-k+1>=0){
+          result[i-k+1]=nums[deque.peekFirst()];
         }
       }
       return result;
@@ -46,4 +47,3 @@ public class MaxOfKSlidingWindows {
 //      System.out.println(maxWindows( new int[] {2,4,3,7,8,6,5,16,19,33,32,34,35}, 5));
     }
   }
-  
